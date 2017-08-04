2e5583c4153a9d1ac104c0418ab0e01220279d97
kulki-rahul <rahul.kulkarni@outlook.com>
2017-08-04 10:53:36 +0000

    Update Test-Deployment.ps1

--- Test-Deployment.ps1
+++ Test-Deployment.ps1
@@ -1,1 +1,1 @@
-az network application-gateway address-pool update --gateway-name rkulkarni -n appGatewayBackendPool -g devOpsDemo --servers [Environment]::GetEnvironmentVariable("DeploymentIPAddress","rkulkarni")
+az network application-gateway address-pool update --gateway-name rkulkarni -n appGatewayBackendPool -g devOpsDemo --servers [Environment]::GetEnvironmentVariable("DeploymentIPAddress","User")
