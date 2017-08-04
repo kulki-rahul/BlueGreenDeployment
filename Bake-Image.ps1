e91067144bfc56bc3fc171147cf5b36430838af6
kulki-rahul <rahul.kulkarni@outlook.com>
2017-08-04 10:03:50 +0000

    Update Bake-Image.ps1

--- Bake-Image.ps1
+++ Bake-Image.ps1
@@ -1,13 +1,19 @@
-﻿
+
+sleep 5
+
 $list = az acr repository show-tags -n rkulkarnidevopsdemo --repository bluegreendeploymentdemo --password +++/==qh9vT+L=baPyO/+Btnj=5WExk/ --resource-group devOpsDemo --username RkulkarnidevOpsdemo -o table
 $list = $list -replace 'v',''
+$count = 0;
 
 $listNumber = foreach($number in $list) {
     try {
+    $count ++
+    if($count -gt 2)
+    {
+        $k = $number.Split(',')[0]
 
-    $k = $number.Split(',')[0]
-
-        [int]::parse($k)
+            [int]::parse($k)
+        }
     }
     catch {
         Invoke-Expression -Command $k;
@@ -16,13 +22,14 @@ $listNumber = foreach($number in $list) {
 
 $listNumber = $listNumber | Sort-Object
 $currentVersion = $listNumber[$listNumber.Count - 1]
+$newVersion = $currentVersion + 1;
 
-(Get-Content docker-compose-v1.yml).replace('[VersionNumber]', $currentVersion) | Set-Content docker-compose-v1.yml
-$depName = "bluegreendeploymentdemo" + $currentVersion;
-(Get-Content docker-compose-v1.yml).replace('[DeploymentName]', $currentVersion) | Set-Content docker-compose-v1.yml
+(Get-Content docker-compose-v1.yml).replace('[VersionNumber]', $newVersion) | Set-Content docker-compose-v1.yml
+$depName = "bluegreendeploymentdemo" + $newVersion;
+(Get-Content docker-compose-v1.yml).replace('[DeploymentName]', $depName) | Set-Content docker-compose-v1.yml
 
 docker login --username=RkulkarnidevOpsDemo --password=+++/==qh9vT+L=baPyO/+Btnj=5WExk/ rkulkarnidevopsdemo.azurecr.io
-$fullDeploymentName = "rkulkarnidevopsdemo.azurecr.io/bluegreendeploymentdemo:v" + $currentVersion;
+$fullDeploymentName = "rkulkarnidevopsdemo.azurecr.io/bluegreendeploymentdemo:v" + $newVersion;
 
 powershell docker tag bluegreendeploymentdemo $fullDeploymentName
 powershell docker push $fullDeploymentName
